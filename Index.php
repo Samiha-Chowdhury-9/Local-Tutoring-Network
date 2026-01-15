f707587a15a67c3cd693e579586d833783ca6b92
Samiha Chowdhury <samiha.chowdhury99@gmail.com>
2026-01-16 00:39:08 +0600

    Initial homepage

--- Index.php
+++ Index.php
@@ -1,5 +1,17 @@
 <?php
 
-
-
 ?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Local Tutoring Network</title>
+        <link rel="stylesheet" href="View/v_css/homeStyle.css">
+    </head>
+    <body>
+        <h1> Welcome to  ... </h1>
+        <button onclick="location.href='View/v_login.php'">Login</button>
+        <button onclick="location.href='View/v_register.php'">Register</button>
+
+    </body>
+</html>
--- View/v_login.php
+++ View/v_login.php
@@ -1,5 +1,27 @@
 <?php
 
-
-
 ?>
+
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/loginStyle.css">
+    </head>
+    <body>
+        <form action="../Controller/c_authentication.php" method="GET">
+            User Id:
+            <input type="text" name="userId" placeholder="input your id here"><br>
+            <span name="idErr"><?php if(isset($_GET["idErr"])){echo $_GET["idErr"];}?></span><br>
+            Password:
+            <input type="password" name="pass"><br>
+            <span name="passErr"><?php if(isset($_GET["passErr"])){echo $_GET["passErr"];}?></span><br><br>
+            <input type="submit" name="submit" value="submit">
+            <input type="reset" name="reset" value="reset">
+
+
+
+        </form>
+        <span name="genErr"><?php if(isset($_GET["genErr"])){echo $_GET["genErr"];}?></span>
+
+    </body>
+</html>
