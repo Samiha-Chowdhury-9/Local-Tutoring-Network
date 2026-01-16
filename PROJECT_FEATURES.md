327f9e11d1d002e58b09d3eee0568cd63c8baaff
Samiha Chowdhury <samiha.chowdhury99@gmail.com>
2026-01-16 19:04:12 +0600

    Updated login

--- PROJECT_FEATURES.md
+++ PROJECT_FEATURES.md
@@ -1,1 +1,2 @@
 # Implemented Features
+## T1 : Made home page with login and registration
--- View/v_login.php
+++ View/v_login.php
@@ -9,12 +9,17 @@
     </head>
     <body>
         <form action="../Controller/c_authentication.php" method="GET">
-            User Id:
-            <input type="text" name="userId" placeholder="input your id here"><br>
+            Name:
+            <input type="text" id="name" name="name"><br>
             <span name="idErr"><?php if(isset($_GET["idErr"])){echo $_GET["idErr"];}?></span><br>
             Password:
-            <input type="password" name="pass"><br>
-            <span name="passErr"><?php if(isset($_GET["passErr"])){echo $_GET["passErr"];}?></span><br><br>
+            <input type="password" id="password" name="password"><br>
+            <span name="passErr"><?php if(isset($_GET["passErr"])){echo $_GET["passErr"];}?></span><br>
+             Role:
+            <input type="checkbox" id="tutor" name="role" value="tutor">Tutor
+            <input type="checkbox" id="student-guardian" name="role" value="student-guardian">Student/Guardian<br>
+            <span name="roleErr"><?php if(isset($_GET["roleErr"])){echo $_GET["roleErr"];}?></span><br><br>
+
             <input type="submit" name="submit" value="submit">
             <input type="reset" name="reset" value="reset">
 
