ebe21546970f2a37588607c10b284a04fe1baa7b
Samiha Chowdhury <samiha.chowdhury99@gmail.com>
2026-01-21 22:53:10 +0600

    DB update

---
+++ ltn.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 21, 2026 at 05:52 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ltn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session_schedules`
+--
+
+CREATE TABLE `session_schedules` (
+  `id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time_slot` varchar(50) NOT NULL,
+  `status` enum('available','booked') NOT NULL DEFAULT 'available'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `session_schedules`
+--
+
+INSERT INTO `session_schedules` (`id`, `tutor_id`, `date`, `time_slot`, `status`) VALUES
+(1, 3, '2026-01-27', '09:00 AM', 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_profiles`
+--
+
+CREATE TABLE `student_profiles` (
+  `profile_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `education_background` varchar(255) DEFAULT NULL,
+  `institution` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_profiles`
+--
+
+INSERT INTO `student_profiles` (`profile_id`, `user_id`, `education_background`, `institution`, `location`) VALUES
+(2, 2, 'class 9', 'preparatory grammar school', 'mirpur'),
+(3, 4, 'class 9', 'chandapur school', 'chondronogor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `subject_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subject_name`) VALUES
+(2, 'Chemistry'),
+(1, 'Math');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor_profiles`
+--
+
+CREATE TABLE `tutor_profiles` (
+  `profile_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `education_background` varchar(255) DEFAULT NULL,
+  `institution` varchar(255) DEFAULT NULL,
+  `experience` varchar(50) DEFAULT NULL,
+  `subjects` varchar(255) DEFAULT NULL,
+  `short_bio` text DEFAULT NULL,
+  `hourly_rate` decimal(10,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tutor_profiles`
+--
+
+INSERT INTO `tutor_profiles` (`profile_id`, `user_id`, `education_background`, `institution`, `experience`, `subjects`, `short_bio`, `hourly_rate`) VALUES
+(1, 3, 'undergraduate', 'nsu', '2 year', 'Chemistry, Math', '', 0.00),
+(2, 5, 'undergraduate', 'aiub', '1 year', 'ict, math', 'pagla', 0.00),
+(3, 6, 'undergraduate', 'brac', '0 year', 'math', 'nothing', 0.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `role` enum('admin','tutor','student-guardian') NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `status`) VALUES
+(1, 'admin', 'admin123', 'admin@proton.co', 'admin', 'active'),
+(2, 'student', 'student123', 'student@proton.com', 'student-guardian', 'active'),
+(3, 'tutor', 'tutor123', 'tutor@proton.com', 'tutor', 'active'),
+(4, 'chandu', 'cchandu123', 'chandu@yahoo.com', 'student-guardian', 'active'),
+(5, 'haradhon', 'haradhon123', 'haradhon@yahoo.com', 'tutor', 'active'),
+(6, 'tutor1', 'tutor123', 'tutor@gmail.com', 'tutor', 'pending');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `session_schedules`
+--
+ALTER TABLE `session_schedules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_new_tutor_id` (`tutor_id`);
+
+--
+-- Indexes for table `student_profiles`
+--
+ALTER TABLE `student_profiles`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD KEY `fk_student_id` (`user_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subject_name` (`subject_name`);
+
+--
+-- Indexes for table `tutor_profiles`
+--
+ALTER TABLE `tutor_profiles`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD KEY `fk_tutor_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `session_schedules`
+--
+ALTER TABLE `session_schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `student_profiles`
+--
+ALTER TABLE `student_profiles`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tutor_profiles`
+--
+ALTER TABLE `tutor_profiles`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `session_schedules`
+--
+ALTER TABLE `session_schedules`
+  ADD CONSTRAINT `fk_new_tutor_id` FOREIGN KEY (`tutor_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student_profiles`
+--
+ALTER TABLE `student_profiles`
+  ADD CONSTRAINT `fk_student_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tutor_profiles`
+--
+ALTER TABLE `tutor_profiles`
+  ADD CONSTRAINT `fk_tutor_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
